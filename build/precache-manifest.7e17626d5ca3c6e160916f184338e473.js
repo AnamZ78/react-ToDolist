df9092c16dc86fc0a37ac5afed8c7c8ab010538d
Anam <anamzahid126@gmail.com>
2023-03-27 11:30:27 +0530

    first commit

---
+++ build/precache-manifest.7e17626d5ca3c6e160916f184338e473.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59e11c4b2a2c9939a7eaa227f8c483fc",
+    "url": "/AnamZ78/Todo-list.git/index.html"
+  },
+  {
+    "revision": "a2d12905c0b569165bba",
+    "url": "/AnamZ78/Todo-list.git/static/js/2.61ae90a2.chunk.js"
+  },
+  {
+    "revision": "2b5aa5cabd4b33f3ef7b",
+    "url": "/AnamZ78/Todo-list.git/static/js/main.64c83cc2.chunk.js"
+  },
+  {
+    "revision": "1d6357622a3457abfd56",
+    "url": "/AnamZ78/Todo-list.git/static/js/runtime-main.0237fc50.js"
+  }
+]);
